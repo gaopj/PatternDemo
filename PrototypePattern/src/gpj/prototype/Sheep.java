fe630873c60f537df20d01c06150e632c6a63133
gaopiji <448030133@qq.com>
2017-04-22 19:12:33 +0800

    完成原型设计模式demo

---
+++ PrototypePattern/src/gpj/prototype/Sheep.java
@@ -0,0 +1,43 @@
+package gpj.prototype;
+
+import java.util.Date;
+
+import javax.xml.crypto.Data;
+
+/**
+ * 浅复制
+ * @author gpj
+ *
+ */
+public class Sheep implements Cloneable{
+
+	
+	public Sheep(String name, Date birthday) {
+		super();
+		this.name = name;
+		this.birthday = birthday;
+	}
+	public Sheep() {
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Date getBirthday() {
+		return birthday;
+	}
+	public void setBirthday(Date birthday) {
+		this.birthday = birthday;
+	}
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		Object object = super.clone();//直接调用Object的clone方法
+		return object;
+	}
+	private String name;
+	private Date birthday;
+	
+	
+}
